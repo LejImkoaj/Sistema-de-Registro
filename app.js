c09696bc0ba07809b828bc0ee5bd3801ea266f1a
LejImkoaj <dani05sm@gmail.com>
2025-08-11 16:46:27 -0500

    Agregar Archivos del Challenge

--- app.js
+++ app.js
@@ -1,1 +1,1 @@
-console.log("Ejecutando el Sistema");
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
